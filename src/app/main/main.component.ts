cdf9d76b05e068aa54041d36f65ec585c9b34ed2
Erdi Köse <erdi@algebra.global>
2019-09-10 17:19:37 +0300

    content wrapper height

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,25 +1,38 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
   public sidebarMenuCollapsed = false;
+  @ViewChild("contentWrapper", { static: false }) contentWrapper;
 
   constructor(private renderer: Renderer2) {}
 
   ngOnInit() {}
 
+  mainSidebarHeight(height) {
+    this.renderer.setStyle(
+      this.contentWrapper.nativeElement,
+      "min-height",
+      height + "px"
+    );
+    console.log(
+      "mainSidebarHeight",
+      this.contentWrapper.nativeElement.offsetHeight
+    );
+  }
+
   toggleMenuSidebar() {
     if (this.sidebarMenuCollapsed) {
-      this.renderer.removeClass(document.body, 'sidebar-open');
-      this.renderer.addClass(document.body, 'sidebar-collapse');
+      this.renderer.removeClass(document.body, "sidebar-open");
+      this.renderer.addClass(document.body, "sidebar-collapse");
       this.sidebarMenuCollapsed = false;
     } else {
-      this.renderer.removeClass(document.body, 'sidebar-collapse');
-      this.renderer.addClass(document.body, 'sidebar-open');
+      this.renderer.removeClass(document.body, "sidebar-collapse");
+      this.renderer.addClass(document.body, "sidebar-open");
       this.sidebarMenuCollapsed = true;
     }
   }
--- src/app/main/menu-sidebar/menu-sidebar.component.ts
+++ src/app/main/menu-sidebar/menu-sidebar.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from 'src/app/utils/services/app.service';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Renderer2,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import { AppService } from "src/app/utils/services/app.service";
 
 @Component({
-  selector: 'app-menu-sidebar',
-  templateUrl: './menu-sidebar.component.html',
-  styleUrls: ['./menu-sidebar.component.scss']
+  selector: "app-menu-sidebar",
+  templateUrl: "./menu-sidebar.component.html",
+  styleUrls: ["./menu-sidebar.component.scss"]
 })
-export class MenuSidebarComponent implements OnInit {
+export class MenuSidebarComponent implements OnInit, AfterViewInit {
+  @ViewChild("mainSidebar", { static: false }) mainSidebar;
+  @Output() mainSidebarHeight: EventEmitter<any> = new EventEmitter<any>();
   constructor(public appService: AppService) {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.mainSidebarHeight.emit(this.mainSidebar.nativeElement.offsetHeight);
+  }
 }
