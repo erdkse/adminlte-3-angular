22b1544ac3f82448842ec13b162d158e5e411892
Erdi Köse <me@erdkse.com>
2019-09-12 16:14:05 +0300

    fix navigation drawer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
-  public sidebarMenuCollapsed = false;
-  @ViewChild('contentWrapper', { static: false }) contentWrapper;
+  public sidebarMenuOpened = true;
+  @ViewChild("contentWrapper", { static: false }) contentWrapper;
 
   constructor(private renderer: Renderer2) {}
 
@@ -19,21 +19,18 @@ export class MainComponent implements OnInit {
     //   'min-height',
     //   height - 114 + 'px'
     // );
-    console.log(
-      'mainSidebarHeight',
-      this.contentWrapper.nativeElement.offsetHeight
-    );
   }
 
   toggleMenuSidebar() {
-    if (this.sidebarMenuCollapsed) {
-      this.renderer.removeClass(document.body, 'sidebar-open');
-      this.renderer.addClass(document.body, 'sidebar-collapse');
-      this.sidebarMenuCollapsed = false;
+    console.log("sidebarMenuCollapsed", this.sidebarMenuOpened);
+    if (this.sidebarMenuOpened) {
+      this.renderer.removeClass(document.body, "sidebar-open");
+      this.renderer.addClass(document.body, "sidebar-collapse");
+      this.sidebarMenuOpened = false;
     } else {
-      this.renderer.removeClass(document.body, 'sidebar-collapse');
-      this.renderer.addClass(document.body, 'sidebar-open');
-      this.sidebarMenuCollapsed = true;
+      this.renderer.removeClass(document.body, "sidebar-collapse");
+      this.renderer.addClass(document.body, "sidebar-open");
+      this.sidebarMenuOpened = true;
     }
   }
 }
