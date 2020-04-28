c461600aceb2d4bcad71690766700b946424e521
Erdkse <me@erdkse.com>
2020-04-28 23:18:48 +0300

    change version

--- src/app/pages/main/footer/footer.component.ts
+++ src/app/pages/main/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from './../../../../../package.json';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  public appVersion = version;
   constructor() {}
 
   ngOnInit() {}
